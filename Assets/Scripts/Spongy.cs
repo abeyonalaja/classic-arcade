18a0292ae5d3ef1cbd4e16358f84e3c25f71816d
Abideen Onalaja <abey.onalaja@gmail.com>
2015-01-24 21:04:40 +0000

    Add Spongy game object and character controller

---
+++ Assets/Scripts/Spongy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spongy : MonoBehaviour {
+
+	public float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveCharacter ();
+	}
+
+	void MoveCharacter() {
+		if(Input.GetKey (KeyCode.A)){
+			transform.Translate(Vector3.left * speed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.D)){
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+		}
+	}
+}
